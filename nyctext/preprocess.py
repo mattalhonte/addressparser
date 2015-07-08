147e02a1c1bfad40684ac5058e796dc069f4a2dd
matt <mattalhonte@gmail.com>
2015-07-08 16:29:11 -0400

    Successful merge between unicode-fixed dev and implied-street functions

--- nyctext/preprocess.py
+++ nyctext/preprocess.py
@@ -34,9 +34,63 @@ def filter_blockcodes(text):
     _b = 'brooklyn|bronx|manhattan|staten\s+island|queens'
     _rex_blockcodes = "BOROUGH\s+of\s+(%s)\s+[^b]{7,15}Block\s+\d+,\slot\s\d+.\s*" % _b
     _rex_blockcodes = re.compile(_rex_blockcodes, re.I)
-
     return '.\n'.join([para for para in _rex_blockcodes.split(text)])
 
+# _street_abbreviations = re.compile('\s+(str?\.?)[\s,]', re.IGNORECASE)
+# _avenue_abbreviations = re.compile('\s+(ave?\.?)[\s,]', re.IGNORECASE)
+# _boulevard_abbreviations = re.compile('\s+(blvd?\.?)[\s,]', re.IGNORECASE)
+# _plaza_abbreviations = re.compile('\s+(plz?\.?)[\s,]', re.IGNORECASE)
+# _drive_abbreviations = re.compile('\s+(dr?\.?)[\s,]', re.IGNORECASE)
+# _parkway_abbreviations = re.compile('\s+(pkwy?\.?)[\s,]', re.IGNORECASE)
+# _road_abbreviations = re.compile('\s+(rd\.?)[\s,]', re.IGNORECASE)
+
+
+# def filter_street_abbreviations(text):
+    # Todo: Build a more comprehensive list of throughways.
+    # See: http://www.semaphorecorp.com/cgi/abbrev.html
+
+    # global _street_abbreviations, _avenue_abbreviations
+    # text = _street_abbreviations.sub(' Street ', text)
+    # text = _avenue_abbreviations.sub(' Avenue ', text)
+    # text = _boulevard_abbreviations.sub(' Boulevard ', text)
+    # text = _plaza_abbreviations.sub(' Plaza ', text)
+    # text = _drive_abbreviations.sub(' Drive ', text)
+    # text = _parkway_abbreviations.sub(' Parkway ', text)
+    # text = _road_abbreviations.sub(' Road ', text)
+    # return text
+    #
+
+#The Street Name Dictionary uses 'NORTH' rather than 'N' or 'N.' or any other variation on that theme.  Interestingly, this is
+#actually inconsistent with city signage - there is no streetsign that says "NORTH 1", it reads "N 1".  Still, streetsigns 
+#are not machine-readable, while the DOT's SND is nice and digitized
+def expand_directions(myString):
+    myString = re.sub(r"""(?<=\s)(N\.?)(?=\s)""", "NORTH", myString)
+    myString = re.sub(r"""(?<=\s)(S\.?)(?=\s)""", "SOUTH", myString)
+    myString = re.sub(r"""(?<=\s)(E\.?)(?=\s)""", "EAST", myString)
+    myString = re.sub(r"""(?<=\s)(W\.?)(?=\s)""", "WEST", myString)
+    return myString
+
+#Street name dictionary doesn't give suffixes to digits (though it does give it to spelled-out words, so we get "FIFTH AVENUE" and 
+#"5 AVENUE", but not "5TH AVENUE").  This is again inconsistent with most signage, buuut machine-readable
+def remove_number_suffixes(myString):
+    return re.sub(r"""(\d)(st|nd|rd|th)""", r"""\1""", myString)
+
+#No reason to have a space before a comma, in NYC addresses or otherwise
+def no_space_commas(myString):
+    return myString.replace(r""" ,""", r""",""")
+
+# regex for streets with a direction but not the word "street". Works on addresses that have already been extracted, then extracts the street name
+numberStreet = re.compile(r"""(?<=\d\s) #It'll be one space away from a digit
+                              (?P<Direction>North|South|East|West) #It has a direction (either written or abbreviated)
+                              (?P<streetNumber>\s\d{1,3}) #Street number
+                              (?P<borAndState>(.*)(?<!Street),\s(New\sYork|Brooklyn|Queens|Staten\s+Island|Bronx),\s(New\sYork|NY))""",
+                              re.IGNORECASE | re.X)
+
+#Let's try and add the word "street" in a few places it's clearly implied
+def add_implied_street_to_dir_street(myString):
+    return re.sub(numberStreet, r"""\1\2 STREET\3""", myString)
+
+_ny_ny = re.compile('(new\s+york|NY)[\s,]+(new\s+york|NY)\s?', re.IGNORECASE)
 
 def filter_ny_ny(text):
     _ny_ny = r"\b((new\s*york|ny)\b[\s,]*)\b(new\s*york|ny)\b"
@@ -90,4 +144,60 @@ def prepare_text(text, verbose=False):
     if verbose:
         print 'filter_neighborhoods:\n\t%s\n' % text
 
+    text = expand_directions(text)
+    if verbose:                                         
+        print 'expand_directions:\n\t%s\n' % text
+
+    text = remove_number_suffixes(text)
+    if verbose:                                         
+        print 'remove_number_suffixes:\n\t%s\n' % text
+  
+    text = no_space_commas(text)
+    if verbose:                                         
+        print 'no_space_commas:\n\t%s\n' % text
+
+    text = add_implied_street_to_dir_street(text)
+    if verbose:                                         
+        print 'add_implied_street_to_dir_street:\n\t%s\n' % text
+
     return text
+
+def location_to_string(tree):
+    return ' '.join([c[0] for c in tree]).replace(' ,', ',')
+
+
+# Some filters for other address formats
+# For instances such as "22 Reade Street, Spector Hall, Borough of Manhattan"
+# boroughOf = re.compile(r"""(Borough\s+of\s+)
+#                            (Brooklyn|Queens|Staten\s+Island|Bronx)""",
+#                            re.IGNORECASE | re.VERBOSE)
+#
+# boroughOfManhattan = re.compile(r"""(Borough\s+of\s+)
+#                            (Manhattan)""",
+#                            re.IGNORECASE | re.VERBOSE)
+#
+# def filterBoroughOf(text):
+#     return re.sub(boroughOf, r"""\g<2>, NY""", text)
+#
+# def filterBoroughOfManhattan(text):
+#     return re.sub(boroughOfManhattan, r"""New York, NY""", text)
+#
+#
+# # For instances such as "1 Centre Street in Manhattan"
+#
+# inBorough = re.compile(r"""(\sin\s)
+#                            (Brooklyn|Queens|Staten\s+Island|Bronx)""",
+#                            re.IGNORECASE | re.VERBOSE)
+#
+#
+# inManhattan = re.compile(r"""(\sin\s)
+#                            (Manhattan)""",
+#                            re.IGNORECASE | re.VERBOSE)
+#
+#
+# def filterInBorough(text):
+#     return re.sub(inBorough, r', \g<2>, NY', text)
+#
+#
+# def filterInManhattan(text):
+#     return re.sub(inManhattan, r', New York, NY', text)
